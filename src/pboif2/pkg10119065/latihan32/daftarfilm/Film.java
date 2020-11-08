5079c70ff415165dd09cbda482c9c080e9362d16
fachriansyahmni <fachriansyahmni@gmail.com>
2020-11-08 08:09:31 +0700

    update

--- src/pboif2/pkg10119065/latihan32/daftarfilm/Film.java
+++ src/pboif2/pkg10119065/latihan32/daftarfilm/Film.java
@@ -1,8 +1,12 @@
 package pboif2.pkg10119065.latihan32.daftarfilm;
 
-/**
- *
- * @author Fachriansyah PC
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan program daftar film 
+                          dengan objek
  */
 public class Film {
     String filmName, filmGenre;
